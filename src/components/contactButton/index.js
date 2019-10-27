271dd629d80216271f6853d36eaa0cfe9bc86926
Max Baldwin <maxrbaldwin2328@gmail.com>
2019-10-27 13:42:51 -0400

    an actual button and styling

--- src/components/contactButton/index.js
+++ src/components/contactButton/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { Link } from 'gatsby';
 
 const Button = styled.button`
+  padding: 10px 20px;
+  background-color: black;
+  font-family: 'PT Serif';
+  width: 30%;
+  align-self: center;
+  border: 2px solid #fff;
+`;
+
+const ButtonItemAnchor = styled(props => <Link {...props} />)`
+  color: white;
+  text-decoration: none;
 `;
 
 const ContactButton = () => (
   <Button>
-    <Link to='/contact'>Get Info</Link>
+    <ButtonItemAnchor to='/contact'>Get Info</ButtonItemAnchor>
   </Button>
 );
 
