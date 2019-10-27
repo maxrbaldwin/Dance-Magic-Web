685cbe0f714df7d219d1ff68167f61e4b5bf5d30
Max Baldwin <maxrbaldwin2328@gmail.com>
2019-10-27 13:44:37 -0400

    A theme provider

---
+++ src/components/withThemeProvider/index.js
@@ -0,0 +1,43 @@
+import React, { createContext } from "react"
+import PropTypes from "prop-types"
+import { ThemeProvider } from "styled-components"
+
+const getViewport = () => {
+  let localViewport
+
+  if (window.matchMedia("(max-width: 700px)").matches) {
+    localViewport = "small"
+  } else if (window.matchMedia("(max-width: 900px)").matches) {
+    localViewport = "medium"
+  } else {
+    localViewport = "large"
+  }
+  return localViewport
+}
+
+function withThemeProvider(WrappedComponent) {
+  const theme = {
+    viewport: getViewport(),
+  }
+  return class extends React.Component {
+    render() {
+      return (
+        <ThemeProvider theme={theme}>
+          <WrappedComponent />
+        </ThemeProvider>
+      )
+    }
+  }
+}
+
+withThemeProvider.defaultProps = {
+  children: {},
+  viewport: "large",
+}
+
+withThemeProvider.propTypes = {
+  children: PropTypes.node,
+  viewport: PropTypes.string.isRequired,
+}
+
+export default withThemeProvider
