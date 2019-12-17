fdfa7a190c91b0a97b516f91d5f0af64dd01fe4b
Max Baldwin <maxrbaldwin2328@gmail.com>
2019-12-17 16:06:50 -0500

    different implementation

--- src/utils/recaptcha.js
+++ src/utils/recaptcha.js
@@ -1,66 +1,14 @@
-import axios from "axios"
-import getHost from "@utils/getHost"
-
-const url = `${getHost()}/api/recaptcha`
-
-const defaultResponse = {
-  data: {
-    error: {
-      message: 'Cannot verify at this time'
-    }
-  }
-}
-
-const fetchUserToken = action => {
+const fetchRecaptchaToken = () => {
   let token = null;
   const grecaptcha = window.grecaptcha || {}
 
   return new Promise(resolve => {
+    if (!process.env.RECAPTCHA_CLIENT) return resolve(token)
     grecaptcha.ready(async () => {
-      token = await grecaptcha.execute(process.env.RECAPTCHA_CLIENT, { action })
-      resolve(token);
+      token = await grecaptcha.execute(process.env.RECAPTCHA_CLIENT, { action: 'contact' })
+      return resolve(token);
     })
   }) 
 }
 
-const fetchVerification = async token => {
-  let response = 0;
-
-  try {
-    const res = await axios({
-      method: 'POST',
-      url,
-      data: { token }
-    })
-    response = res.data
-  } catch (err) {
-    response = err.message === 'Network Error' ? defaultResponse : err.response;
-  }
-
-  return response
-}
-
-const isUserVerified = async action => {
-  // if the environment fails to place this env var, don't hold up the user
-  if (!process.env.RECAPTCHA_CLIENT) return true
-
-  let isVerified = false;
-  let token = null;
-
-  try {
-    token = await fetchUserToken(action)
-  } catch (tokenErr) {
-    console.log('error fetching recaptcha token: ', tokenErr)
-  }
-
-  if (token) {
-    try {
-      isVerified = await fetchVerification(token)
-    } catch (verificationErr) {
-      console.log('error verifying recaptcha token: ', verificationErr)
-    }
-  }
-  return isVerified && isVerified.isValidUser;
-}
-
-export default isUserVerified
+export default fetchRecaptchaToken
