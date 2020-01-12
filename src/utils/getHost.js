79f2ea403faa5ded6288c0bbaba6ba51e7e39d41
maxrbaldwin <maxrbaldwin2328@gmail.com>
2020-01-12 17:24:08 -0500

    wrong host

--- src/utils/getHost.js
+++ src/utils/getHost.js
@@ -1,3 +1,3 @@
-const getHost = () => process.env.NODE_ENV === 'production' ? `https://dance-magic-259922.appspot.com` : 'http://localhost:9000'
+const getHost = () => process.env.NODE_ENV === 'production' ? `https://dance-magic-api-dot-dance-magic-259922.appspot.com/` : 'http://localhost:9000'
 
 export default getHost
