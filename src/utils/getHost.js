c1e298329484d7e6881595e6b16d462e9e2eb87c
maxrbaldwin <maxrbaldwin2328@gmail.com>
2020-01-12 16:49:05 -0500

    change host

--- src/utils/getHost.js
+++ src/utils/getHost.js
@@ -1,3 +1,3 @@
-const getHost = () => process.env.NODE_ENV === 'production' ? `https://${process.env.HOST}` : 'http://localhost:9000'
+const getHost = () => process.env.NODE_ENV === 'production' ? `https://dance-magic-259922.appspot.com/s` : 'http://localhost:9000'
 
 export default getHost
