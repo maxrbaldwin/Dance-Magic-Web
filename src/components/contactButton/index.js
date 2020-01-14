aafe407e23eb3f14081ab04d661ad96f8829d5f4
maxrbaldwin <maxrbaldwin2328@gmail.com>
2020-01-14 12:05:48 -0500

    viewports

--- src/components/contactButton/index.js
+++ src/components/contactButton/index.js
@@ -3,13 +3,17 @@ import styled from 'styled-components';
 import { Link } from 'gatsby';
 
 const Button = styled.button`
-  padding: ${({ theme }) => theme.viewport === 'small' ? '10px 30px' : '10px 20px'};
+  padding: 10px 20px;
   background-color: black;
   font-family: 'PT Serif';
-  width: ${({ theme }) => theme.viewport === 'small' ? 'auto' : '30%'};
+  width: 30%;
   align-self: center;
   border: 2px solid #fff;
-  font-size: ${({ theme }) => theme.viewport === 'small' ? '20px' : '20px'};
+
+  @media (max-width: 700px) {
+    width: auto;
+    padding: 10px 30px;
+  }
 `;
 
 const ButtonItemAnchor = styled(props => <Link {...props} />)`
