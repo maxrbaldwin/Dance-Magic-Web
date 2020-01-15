ce0e04bdb83aaa3dad937cbf15daff0e7e04b665
maxrbaldwin <maxrbaldwin2328@gmail.com>
2020-01-15 13:55:30 -0500

    a good 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 import Layout from '@components/layout'
 import SEO from '@components/seo'
 
+const notFoundImageSource = 'https://storage.cloud.google.com/dance-magic-images/404.gif?authuser=1'
+
+const NotFoundWrapper = styled.div`
+  height: 100vh;
+  text-align: center;
+  padding: 50px 20px 0 20px;
+`
+
+const NotFoundImage = styled.img`
+  border: 5px solid #fff;
+`
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title='404: Not found' />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <NotFoundWrapper>
+      <h1>404! Page Not Found</h1>
+      <h2>This is not the page you're looking for</h2>
+      <p>Please return to the <Link to="/">homepage</Link></p>
+      <NotFoundImage src={notFoundImageSource} />
+    </NotFoundWrapper>
   </Layout>
 )
 
