26cc8df0f36f7ae20fb0f7b808a260b367872304
Max Baldwin <maxrbaldwin2328@gmail.com>
2021-07-28 11:34:57 -0400

    embed google form iframe in registration page

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -1,16 +1,34 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import styled from 'styled-components'
 
+import Layout from '@components/layout'
 import SEO from '@components/seo'
+import MaxWidth from "@styles/maxWidth"
 
-const registrationFormURL = 'https://forms.gle/WsqNSNqMyanbiWSL6'
+const RegistrationIframe = styled.iframe`
+  min-height: 2350px;
+  width: 100%;
+`
 
-const Registration = () => {
-  useEffect(() => {
-    window.location.href = registrationFormURL
-  }, [])
-  return (
+const RegistrationWrapper = styled.div`
+  margin-top: 40px;
+`
+
+const iframeSrc = 'https://docs.google.com/forms/d/e/1FAIpQLSf5H5EM7Z6ws-7rUGue_0Bn4AwWWwvV-u_OscaGhZ9EfYxD2w/viewform?embedded=true'
+
+const Registration = () => (
+  <Layout>
     <SEO title='Dance Magic Registration' />
-  )
-}
+    <RegistrationWrapper>
+      <MaxWidth>
+        <RegistrationIframe
+          frameBorder={0}
+          src={iframeSrc}>
+          Loading…
+        </RegistrationIframe>
+      </MaxWidth>
+    </RegistrationWrapper>
+  </Layout>
+)
 
 export default Registration
